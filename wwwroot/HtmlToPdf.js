9ca39d10ef31e1cf294a43fab3ff8d5087af43a0
keveonwhite <132286186+keveonwhite@users.noreply.github.com>
2024-03-22 16:10:15 -0500

    Connected to SharePoint for each user. Took too long in execution

--- wwwroot/HtmlToPdf.js
+++ wwwroot/HtmlToPdf.js
@@ -5,6 +5,10 @@ async function generateAndDownloadPdf(html, filename) {
         format: 'letter'
     });
 
+    document.getElementsByClassName(".table").css("width:", "97%");
+    document.getElementsByClassName(".table-row").css("width:", "95%");
+    document.getElementsByClassName(".table-data").css("width:", "15%");
+
     return new Promise((resolve, reject) => {
         doc.html(html, {
             callback: doc => {
